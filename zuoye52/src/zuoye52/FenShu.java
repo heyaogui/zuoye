064e50ccd53dbc202fd4f161a0964199da98309c
heyaogui <211920106@qq.com>
2018-05-02 15:15:56 +0800

    5.2zuoye

--- zuoye52/src/zuoye52/FenShu.java
+++ zuoye52/src/zuoye52/FenShu.java
@@ -1,17 +1,19 @@
 package zuoye52;
-
+import java.util.Scanner;
 public class FenShu {
 
 	public static void main(String[] args) {
-		int score=80;
+		Scanner scanner = new  Scanner (System.in);
+		System.out.println("请输入目前的成绩:  ");
+		int score = scanner.nextInt();
 		if(score>=90) {
 			System.out.println("优秀");
 		}
 			else if(score>=80) {
-				System.out.println("中等");
+				System.out.println("良好");
 			}
 			else if(score>=70) {
-				System.out.println("良好");
+				System.out.println("中等");
 			}
 			else {    
 	            System.out.println("差");
--- zuoye52/src/zuoye52/RunNian.java
+++ zuoye52/src/zuoye52/RunNian.java
@@ -1,9 +1,11 @@
 package zuoye52;
-
+import java.util.Scanner;
 public class RunNian {
 
 	public static void main(String[] args) {
-		int year = 2008;
+		Scanner scanner = new  Scanner (System.in);
+		System.out.println("请输入目前的年份:  ");
+		int year = scanner.nextInt();
 		if (year % 400 == 0 || year % 4 == 0) {
 			System.out.println("是闰年");
 		} else if (year % 100 != 0) {
